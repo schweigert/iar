e3ec506dd2cd5be51d65357b208457e661d74a9a
Marlon Henry Schweigert <fleyhe0@gmail.com>
2018-04-01 16:00:25 -0300

    Article

---
+++ ants/tex/ants.py
@@ -0,0 +1,10 @@
+def think():
+    walk()
+    if garbagging():
+        if random() > relative_density():
+            if cel_is_clear():
+                put_dead_ant()
+    else:
+        if random() < relative_density():
+            if not cel_is_clear():
+                get_dead_ant()
